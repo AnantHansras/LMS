4063cb7b985e730fd47b9e8d11f75ee5fff5d39d
AnantHansras <ananthansras.16@gmail.com>
2025-03-31 11:35:07 +0530

    ui changed

--- src/Pages/AllBooks.jsx
+++ src/Pages/AllBooks.jsx
@@ -23,24 +23,8 @@ function AllBooks() {
 
     fetchBooks();
   },[]); // Runs only once when component mounts
-  const [book] = useState([
-    { _id: 1, title: "Machine Learning", author: "J.K. Rowling", genre: "Fantasy" },
-    { _id: 2, title: "A Textbook of Engineering Mechanics", author: "D.S. Kumar", genre: "Engineering" },
-    { _id: 3, title: "Software Engineering", author: "Niranjli Singh, Rakesh Kumar", genre: "Engineering" },
-    { _id: 4, title: "AI Engineering", author: "Chip Huyen", genre: "Artificial Intelligence" },
-    { _id: 5, title: "Aerospace Engineering Beginner's Guide", author: "Andrew L. Long", genre: "Engineering" },
-    { _id: 6, title: "The Art of Prompt Engineering with ChatGPT", author: "Norhan Hunter", genre: "Technology" },
-    { _id: 7, title: "Advanced Object-Oriented Programming", author: "Unknown", genre: "Programming" },
-    { _id: 8, title: "Internet of Things", author: "Dr. Vishal Chaurasiya, Dr. Abhishek Arya", genre: "Technology" },
-    { _id: 9, title: "Cloud Computing", author: "Dr. Neeraj Verma, Deepak Khushwah, Dheeraj Goyal", genre: "Technology" },
-    { _id: 10, title: "Social Engineering: The Science of Human Hacking", author: "Christopher Hadnagy", genre: "Cybersecurity" },
-    { _id: 11, title: "Deep Learning from Scratch", author: "Seth Weidman", genre: "Artificial Intelligence" },
-    { _id: 12, title: "Deep Learning", author: "Dr. Praveen Kumar, Dr. Aastha Parikh", genre: "Artificial Intelligence" },
-  ]);
-
   return (
     <>
-      <Header/>
       <List books={books} />
       <Footer />
     </>
